f11d7f0a3b21e87176a78e6180217b2c369df9d4
Manuel Gonzalez <mgonzalez4648@gmail.com>
2020-03-28 13:11:45 +0100

    Solucionado problema al recargar la página

--- Laboratorio/Proyecto1/funciones.js
+++ Laboratorio/Proyecto1/funciones.js
@@ -3,11 +3,6 @@ var contador = 0;
 
 function addUsuario(textField) {
 	usuarios[contador++] = textField.value;
-	var nombres = "";
-	for (var i = 0; i < usuarios.length; i++) {
-		nombres += usuarios[i];
-	}
-	alert(nombres);
 }
 
 function buscarUsuario(textField) {
