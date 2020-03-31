d60ea0b4f5cc513a904dd06c260c90f08cd65300
Manuel Gonzalez <mgonzalez4648@gmail.com>
2020-03-31 10:36:23 +0200

    Implementada funcionalidad para añadir un usuario

--- Laboratorio/Proyecto1/funciones.js
+++ Laboratorio/Proyecto1/funciones.js
@@ -2,12 +2,21 @@ var usuarios = new Array();
 var contador = 0;
 
 function addUsuario(textField) {
-	usuarios[contador++] = textField.value;
-	var nombres = "";
-	for (var i = 0; i < usuarios.length; i++) {
-		nombres += usuarios[i];
-	}
-	alert(nombres);
+	// Se añade al array
+	usuarios[contador] = textField.value;
+
+	// El nuevo usuario se añade al final de la tabla
+	var table = document.getElementById('tablaUsuarios');
+	var newRow = table.insertRow(table.rows.length - 1);
+	
+	var cell1 = newRow.insertCell(0);
+	var cell2 = newRow.insertCell(1);
+
+	cell1.innerHTML = contador;
+	cell2.innerHTML = textField.value;
+
+	// El número de usuarios aumenta
+	contador++;
 }
 
 function buscarUsuario(textField) {
