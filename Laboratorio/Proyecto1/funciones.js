9c3e4d20e7589ea65fd17d798c59867c15e7c777
Manuel Gonzalez <mgonzalez4648@gmail.com>
2020-03-31 11:27:22 +0200

    Finalizado Laboratorio 1

--- Laboratorio/Proyecto1/funciones.js
+++ Laboratorio/Proyecto1/funciones.js
@@ -1,24 +1,58 @@
 var usuarios = new Array();
-var contador = 0;
+var numUsuarios = 0;
+var selectedCell = null;
 
 function addUsuario(textField) {
+	var nombre = textField.value;
+
+	// Si el nombre está vacio no se añade
+	if (nombre === "") {
+		return;
+	}
+
 	// Se añade al array
-	usuarios[contador] = textField.value;
+	usuarios[numUsuarios] = nombre;
 
 	// El nuevo usuario se añade al final de la tabla
 	var table = document.getElementById('tablaUsuarios');
-	var newRow = table.insertRow(table.rows.length - 1);
+	var newRow = table.insertRow(table.rows.length);
 	
 	var cell1 = newRow.insertCell(0);
 	var cell2 = newRow.insertCell(1);
 
-	cell1.innerHTML = contador;
-	cell2.innerHTML = textField.value;
+	cell1.innerHTML = numUsuarios;
+	cell2.innerHTML = nombre;
 
 	// El número de usuarios aumenta
-	contador++;
+	numUsuarios++;
 }
 
+// Devuelve la posición del primer usuario que encuentra en el array
 function buscarUsuario(textField) {
-	alert("Buscar");
+	var nombre = textField.value;
+
+	// Si el nombre está vacio no se añade
+	if (nombre === "") {
+		return;
+	}
+
+	// Busqueda del nombre
+	var i = 0;
+	while (i < numUsuarios && usuarios[i] != nombre) i++;
+
+	// Si se ha encontrado se resalta en la tabla
+	if (i < numUsuarios) {
+		// Se desresalta la celda anterior
+		if (selectedCell != null) {
+			selectedCell.style.backgroundColor = "";
+		}
+
+		// Se resalta la celda en la tabla
+		selectedCell = document.getElementById('tablaUsuarios').rows[i + 1].cells[0]; // La fila de la cabecera no cuenta
+		selectedCell.style.backgroundColor = "#FF9F00";
+	} else {
+		alert("Usuario \"" + nombre + "\" no encontrado");
+	}
+
+
 }
