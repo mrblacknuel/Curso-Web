ba863a35dd0780c162a10366f444b59b82d768f2
Manuel Gonzalez <mgonzalez4648@gmail.com>
2020-05-28 15:24:51 +0200

    Finalizado generador aleatorio de nombres

--- SeccionJS/NameGenerator/js/app.js
+++ SeccionJS/NameGenerator/js/app.js
@@ -6,21 +6,35 @@ function generarNombres() {
     const origen = document.getElementById("origen").value;
     const genero = document.getElementById("genero").value;
     const numero = document.getElementById("numero").value;
+    let url = `https://randomuser.me/api/?inc=name&results=${numero}`;
 
-    /*if (origen == "" || genero == "" || numero == "") {
-        alert("Rellene todos los campos para poder generar el nombre");
-        return;
-    }*/
+    if (origen != "") {
+        url += `&nat=${origen}`;
+    }
+
+    if (genero != "") {
+        url += `&gender=${genero}`;
+    }
 
     // Conexión con el API
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', `//api.name-fake.com/random/random`, true);
-    xhr.setRequestHeader("Access-Control-Allow-Origin", "null");
-    xhr.onreadystatechange = () => {
-        if (this.status === 200) {
-            console.log("Correcto");
-        } else {
-            console.log("Incorrecto");
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        if (this.status == 200) {
+            let personas = JSON.parse(this.response).results.map((c) => c.name);
+
+            let htmlNombres = '<label for="resultado">Nombres generados:</label>';
+            htmlNombres += '<ul class="lista">';
+
+            // Imprimir cada nombre
+            personas.forEach(function (nombre) {
+                htmlNombres += `
+                                  <li>${nombre.first} ${nombre.last}</li>
+                        `;
+            })
+
+            htmlNombres += '</ul>';
+            panelNombres.innerHTML = htmlNombres;
         }
     };
     xhr.send();
