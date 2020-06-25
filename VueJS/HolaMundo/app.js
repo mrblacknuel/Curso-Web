673083ed95c322949aa0fbef9558b2b9e0717426
Manuel González <mgonzalez4648@gmail.com>
2020-06-25 11:17:17 +0200

    Añadida conexión con V-Model

--- VueJS/HolaMundo/app.js
+++ VueJS/HolaMundo/app.js
@@ -1,11 +1,20 @@
 const app = new Vue({
-    el: '#app',
-    data: {
-        titulo: 'Hola Mundo con Vue',
-        frutas: [
-            {nombre: 'Pera', cantidad: 2},
-            {nombre: 'Plátano', cantidad: 5},
-            {nombre: 'Cereza', cantidad: 0}
-        ]
-    }
-})
+  el: "#app",
+  data: {
+    titulo: "Hola Mundo con Vue",
+    frutas: [
+      { nombre: "Pera", cantidad: 2 },
+      { nombre: "Plátano", cantidad: 5 },
+      { nombre: "Cereza", cantidad: 0 },
+    ],
+    nuevaFruta: "",
+  },
+  methods: {
+    agregarFruta() {
+      this.frutas.push({
+        nombre: this.nuevaFruta,
+        cantidad: 0,
+      });
+    },
+  },
+});
