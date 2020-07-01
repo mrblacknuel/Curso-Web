33208505c6254383747014cce9c54db078b80991
Manuel González <mgonzalez4648@gmail.com>
2020-07-01 16:40:47 +0200

    Inicializado ejercicio de seguimiento del dolar

---
+++ VueJS/dollar-tracker/src/views/Grid.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus eum aut libero? Ducimus laudantium, officiis sapiente, provident ex impedit nesciunt dolorum voluptatibus enim blanditiis reprehenderit expedita consectetur eveniet, cumque quos.</p>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus eum aut libero? Ducimus laudantium, officiis sapiente, provident ex impedit nesciunt dolorum voluptatibus enim blanditiis reprehenderit expedita consectetur eveniet, cumque quos.</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Grid"
+};
+</script>
