0dacf6b2bfe3ff36c447fee81306e2425db7a43e
Manuel González <mgonzalez4648@gmail.com>
2020-07-02 10:47:07 +0200

    Finalizado proyecto de seguimiento del valor del dolar

--- VueJS/dollar-tracker/src/store/index.js
+++ VueJS/dollar-tracker/src/store/index.js
@@ -1,15 +1,24 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    loading: {
+      titulo: "",
+      activo: false,
+    },
   },
   mutations: {
+    mostrarCarga(state, titulo) {
+      state.loading.titulo = titulo;
+      state.loading.activo = true;
+    },
+    ocultarCarga(state) {
+      state.loading.activo = false;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
--- VueJS/dollar-tracker/src/views/Home.vue
+++ VueJS/dollar-tracker/src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-date-picker full-width v-model="fecha" :min="minimo" :max="maximo" @change="getDolar(fecha)"></v-date-picker>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-card color="primary" dark>
+            <v-card-text class="display-1 text-center">1 $ = {{valor}} CLP</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+import { mapMutations } from "vuex";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      fecha: new Date().toISOString().substr(0, 10),
+      minimo: "1984",
+      maximo: new Date().toISOString().substr(0, 10),
+      valor: null
+    };
+  },
+  methods: {
+    ...mapMutations(["mostrarCarga", "ocultarCarga"]),
+    async getDolar(dia) {
+      let arrayFecha = dia.split("-");
+      let ddmmyy = arrayFecha[2] + "-" + arrayFecha[1] + "-" + arrayFecha[0];
+
+      try {
+        // Muestra una pantalla de carga
+        this.mostrarCarga(`Cargando moneda para el ${ddmmyy}...`);
+
+        let resp = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`);
+        if (resp.data.serie.length > 0) {
+          this.valor = await resp.data.serie[0].valor;
+        } else {
+          this.valor = "";
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        // Oculta la pantalla de carga
+        this.ocultarCarga();
+      }
+    }
+  },
+  created() {
+    this.getDolar(this.fecha);
   }
-}
+};
 </script>
